5a9397022e850e772ec737e2722161572ac51d6b
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-10-27 16:50:23 +0530

    Commit

---
+++ practice.js
@@ -0,0 +1,27 @@
+
+
+const { log } = require("console")
+
+let myDate = new Date()
+console.log(myDate.toDateString());
+
+console.log(myDate.toISOString());
+
+console.log(myDate.toJSON());
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleString());
+
+console.log(myDate.toLocaleTimeString());
+
+console.log(myDate.toString());
+
+console.log(myDate.toTimeString());
+
+console.log(myDate.toUTCString());
+
+console.log(myDate.getTimezoneOffset());
+
+console.log(typeof myDate);
+
